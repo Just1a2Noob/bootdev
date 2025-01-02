6729c5d2d97263b744a123dd51a5a48f555042d3
Just1a2Noob <kamikira.hd@gmail.com>
2025-01-02 20:37:44 +0700

    Created tests for split_nodes_link and split_nodes_image

--- static_website_generator/src/test_split_delimiter.py
+++ static_website_generator/src/test_split_delimiter.py
@@ -1,10 +1,11 @@
 import unittest
 
-from split_delimiter import split_nodes_delimiter
+from split_delimiter import split_nodes_delimiter, split_nodes_image, split_nodes_link
 from textnode import TextNode, TextType
 
 
 class test_split_delimiter(unittest.TestCase):
+    # TODO: Add 1 more test with multiple delimiters
     def test_normality(self):
         node1 = split_nodes_delimiter(
             [
@@ -59,6 +60,51 @@ class test_split_delimiter(unittest.TestCase):
                 TextType.ITALIC,
             )
         self.assertEqual(str(cm.exception), "The list must contain TextNode")
+
+
+class test_extract_link(unittest.TestCase):
+    def test_normality(self):
+        node = TextNode(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+            TextType.TEXT,
+        )
+        node2 = [
+            TextNode("This is text with a link ", TextType.TEXT),
+            TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+            TextNode(" and ", TextType.TEXT),
+            TextNode(
+                "to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"
+            ),
+        ]
+        self.assertEqual(split_nodes_link([node]), node2)
+
+    def test_invalid_text_node(self):
+        with self.assertRaises(ValueError) as cm:
+            split_nodes_link(
+                ["This is a youtube [link](https://www.youtube.com)", TextType.TEXT]
+            )
+        self.assertEqual(str(cm.exception), "The list must contain TextNode")
+
+    def test_invalid_old_nodes(self):
+        with self.assertRaises(TypeError) as cm:
+            split_nodes_link(
+                {"link": "https://www.youtube.com", "text_type": TextType.TEXT}
+            )
+        self.assertEqual(str(cm.exception), "old_nodes must be a list")
+
+
+class test_extract_images(unittest.TestCase):
+    def test_normality(self):
+        node = TextNode(
+            "This is an image of a ![cute cat](https://www.imgur.com/cat) she is very cute",
+            TextType.TEXT,
+        )
+        node2 = [
+            TextNode("This is an image of a ", TextType.TEXT),
+            TextNode("cute cat", TextType.LINK, "https://www.imgur.com/cat"),
+            TextNode(" she is very cute", TextType.TEXT),
+        ]
+        self.assertEqual(split_nodes_image([node]), node2)
 
 
 if __name__ == "__main__":
