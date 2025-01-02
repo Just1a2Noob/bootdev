9c955625df6cd2a4a93e5e08b14376ab6accd316
Just1a2Noob <kamikira.hd@gmail.com>
2025-01-02 20:36:38 +0700

    Created split functions for finding links and images in TextNode, but still unfinished

--- static_website_generator/src/split_delimiter.py
+++ static_website_generator/src/split_delimiter.py
@@ -1,5 +1,6 @@
 import re
 
+from extract_links import extract_markdown_images, extract_markdown_links
 from textnode import TextNode, TextType
 
 
@@ -17,6 +18,7 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         if not isinstance(node, TextNode):
             raise ValueError("The list must contain TextNode")
 
+        # TODO: This can only find one link change it to re.findall
         try:
             keyword = re.search(f"{delimiter}(.+?){delimiter}", node.text).group(1)
         except Exception:
@@ -37,5 +39,75 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                         text_type=node.text_type,
                     )
                 )
+    return result
+
+
+def split_nodes_image(old_nodes):
+    if type(old_nodes) is not list:
+        raise TypeError("old_nodes must be a list")
+
+    result = []
+    for node in old_nodes:
+        if not isinstance(node, TextNode):
+            raise ValueError("The list must contain TextNode")
+
+        images = {}
+        for i in extract_markdown_images(node.text):
+            images[i[0]] = i[1]
+
+        original_list = re.split(r"!\[([^\[\]]*)\]\(([^()\s]+)\)", node.text)
+        max_length = len(original_list)
+        for key in images:
+            j = 0
+            while j < max_length:
+                if key in original_list[j]:
+                    result.append(TextNode(key, TextType.LINK, images[key]))
+                    original_list.remove(key)
+                    original_list.remove(images[key])
+                    max_length -= 2
+                    continue
+                else:
+                    result.append(TextNode(original_list[j], TextType.TEXT))
+                    original_list.remove(original_list[j])
 
     return result
+
+
+def split_nodes_link(old_nodes):
+    if type(old_nodes) is not list:
+        raise TypeError("old_nodes must be a list")
+
+    result = []
+    for node in old_nodes:
+        if not isinstance(node, TextNode):
+            raise ValueError("The list must contain TextNode")
+
+        images = {}
+        for i in extract_markdown_links(node.text):
+            images[i[0]] = i[1]
+
+        original_list = re.split(r"(?<!\!)\[([^\[\]]+)\]\(([^()\s]+)\)", node.text)
+        max_length = len(original_list)
+        for key in images:
+            j = 0
+            while j < max_length:
+                if key in original_list[j]:
+                    result.append(TextNode(key, TextType.LINK, images[key]))
+                    original_list.remove(key)
+                    original_list.remove(images[key])
+                    max_length -= 2
+                    break
+                else:
+                    result.append(TextNode(original_list[j], TextType.TEXT))
+                    original_list.remove(original_list[j])
+
+    return result
+
+
+# TODO: Both split functions cannot handle the below test case
+# Fix both functions so that the test case below works
+node = TextNode(
+    "This is an image of a ![cute cat](https://www.imgur.com/cat) she is very cute",
+    TextType.TEXT,
+)
+print(split_nodes_image([node]))
