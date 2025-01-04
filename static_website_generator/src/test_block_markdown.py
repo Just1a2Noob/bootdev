68c8c10c9dc9d390da39e9971365a0c1e67be822
Just1a2Noob <kamikira.hd@gmail.com>
2025-01-04 08:06:44 +0700

    Added test for block_to_block_type function

--- static_website_generator/src/test_block_markdown.py
+++ static_website_generator/src/test_block_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import block_to_block_type, markdown_to_blocks
 
 
 class test_markdown_to_blocks(unittest.TestCase):
@@ -58,6 +58,47 @@ This is the same paragraph on a new line
         node = "  Block with spaces  \n\n   Another block   "
         expected_output = ["Block with spaces", "Another block"]
         self.assertEqual(self.markdown_to_blocks(node), expected_output)
+
+
+class test_block_to_block_type(unittest.TestCase):
+    def test_header_block(self):
+        node = "# This should be a header"
+        expected_output = "header"
+        self.assertEqual(block_to_block_type(node), expected_output)
+
+    def test_code_block(self):
+        node = "```print('hello world')```"
+        expected_output = "code"
+        self.assertEqual(block_to_block_type(node), expected_output)
+
+    def test_unordered_list(self):
+        node1 = "* List1\n* List2\n* List3"
+        expected_output1 = "unordered_list"
+        self.assertEqual(block_to_block_type(node1), expected_output1)
+
+        node2 = "- List1\n- List2\n- List3"
+        expected_output2 = "unordered_list"
+        self.assertEqual(block_to_block_type(node2), expected_output2)
+
+    def test_ordered_list(self):
+        node = "1. List1\n2. List2\n3. List3"
+        expected_output = "ordered_list"
+        self.assertEqual(block_to_block_type(node), expected_output)
+
+    def test_qoute_block(self):
+        node = ">[!NOTE]\n>Very important note."
+        expected_output = "quote"
+        self.assertEqual(block_to_block_type(node), expected_output)
+
+    def test_basic_text(self):
+        node = "This should just be a **normal** *text*"
+        expected_output = "paragraph"
+        self.assertEqual(block_to_block_type(node), expected_output)
+
+    def test_unclosed_delimiter(self):
+        node = "```print('hello world')``"
+        expected_output = "paragraph"
+        self.assertEqual(block_to_block_type(node), expected_output)
 
 
 if __name__ == "__main__":
