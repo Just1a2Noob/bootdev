570e6a486d1061825495e067573ec8e594663706
Just1a2Noob <kamikira.hd@gmail.com>
2025-01-05 11:14:30 +0700

    Overhaul of main.py, it now has copy files function and the main function now has the functionaly to generate a local hosted website

--- static_website_generator/src/main.py
+++ static_website_generator/src/main.py
@@ -1,9 +1,51 @@
-from textnode import TextNode
+import os
+import shutil
+
+from generate_page import generate_pages_recursive
+
+# Change the paths here
+dir_path_static = os.path.expanduser(
+    "~/Documents/github/bootdev/static_website_generator/static/"
+)
+dir_path_public = os.path.expanduser(
+    "~/Documents/github/bootdev/static_website_generator/public/"
+)
+dir_path_content = os.path.expanduser(
+    "~/Documents/github/bootdev/static_website_generator/content"
+)
+template_path = os.path.expanduser(
+    "~/Documents/github/bootdev/static_website_generator/template.html"
+)
+
+
+def copy_files_recursive(source_dir_path, destination_dir_path):
+    if not os.path.exists(destination_dir_path):
+        os.mkdir(destination_dir_path)
+
+    for filename in os.listdir(source_dir_path):
+        from_path = os.path.join(source_dir_path, filename)
+        dest_path = os.path.join(destination_dir_path, filename)
+        print(f" * {from_path} -> {dest_path}")
+
+        # Checks if the from_path is a file
+        if os.path.isfile(from_path):
+            # Then copies it to dest_path
+            shutil.copy(from_path, dest_path)
+        else:
+            # If its not a file recurive call with new from and dest
+            copy_files_recursive(from_path, dest_path)
 
 
 def main():
-    dummy_node = TextNode("This is just text", "text")
-    print(dummy_node.__repr__())
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+    print("Generating content...")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
 
 
 main()
