3cfdffb25c73430087be46c5d8b5e6caf8e9a406
Just1a2Noob <kamikira.hd@gmail.com>
2025-08-07 21:26:42 +0700

    Created pokecache package

--- pokedexcli/internal/pokecache/cache.go
+++ pokedexcli/internal/pokecache/cache.go
@@ -1,22 +1,68 @@
 package pokecache
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
-func cache() map[string]cacheEntry {
-	return map[string]cacheEntry{}
+// Cache -
+type Cache struct {
+	mu    *sync.Mutex
+	cache map[string]cacheEntry
 }
 
-func NewCache(interval time.Duration) (cacheEntry, error) {
-	return cacheEntry{}, nil
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
 }
 
-func (c *cacheEntry) Add(key string, val []byte) {
-	c.val = val
+func NewCache(interval time.Duration) Cache {
+	c := Cache{
+		cache: make(map[string]cacheEntry),
+		mu:    &sync.Mutex{},
+	}
+
+	go c.reapLoop(interval)
+
+	return c
 }
 
-func (c cacheEntry) Get(key string) ([]byte, bool) {
+func (c *Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	cached := cacheEntry{
+		createdAt: time.Now().UTC(),
+		val:       val,
+	}
+
+	defer c.mu.Unlock()
+	c.cache[key] = cached
 }
 
-func (c cacheEntry) reapLoop() error {
-	return nil
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	value, exists := c.cache[key]
+	if exists == false {
+		return nil, false
+	}
+
+	return value.val, true
+}
+
+func (c *Cache) reapLoop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	for range ticker.C {
+		c.reap(time.Now().UTC(), interval)
+	}
+}
+
+func (c *Cache) reap(now time.Time, last time.Duration) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	for k, v := range c.cache {
+		if v.createdAt.Before(now.Add(-last)) {
+			delete(c.cache, k)
+		}
+	}
 }
