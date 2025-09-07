b7c62178372e33c7291f18b0994e6bd2e009cc05
Just1a2Noob <kamikira.hd@gmail.com>
2025-09-07 13:24:21 +0700

    Creating posts command function

--- blog_aggregator/handler_agg.go
+++ blog_aggregator/handler_agg.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/Just1a2Noob/bootdev/blog_aggregator/internal/database"
+	"github.com/google/uuid"
 )
 
 func handlerAgg(s *state, cmd command) error {
@@ -42,8 +43,33 @@ func scrapeFeeds(s *state) error {
 		return fmt.Errorf("Couldn't not get the fetch results : %v", err)
 	}
 
-	for _, title := range feed.Channel.Title {
-		println(title)
+	savePosts(s, feed, fetch)
+
+	return nil
+}
+
+func savePosts(s *state, feed *RSSFeed, fd database.Feed) error {
+	for _, post := range feed.Channel.Item {
+
+		published_at := sql.NullTime{}
+
+		if post.Link == "" {
+			return fmt.Errorf("Post link is empty")
+		}
+
+		_, err := s.db.CreatePost(context.Background(), database.CreatePostParams{
+			ID:          uuid.New(),
+			CreatedAt:   time.Now().UTC(),
+			UpdatedAt:   time.Now().UTC(),
+			Title:       sql.NullString{String: post.Title},
+			Url:         sql.NullString{String: post.Link},
+			Description: sql.NullString{String: post.Description},
+			PublishedAt: published_at,
+			FeedID:      fd.ID,
+		})
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
