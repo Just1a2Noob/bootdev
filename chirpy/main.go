9239d8807754b84eb8abdcfca3d4039ee58637cc
Just1a2Noob <kamikira.hd@gmail.com>
2025-09-08 12:01:42 +0700

    Small Fixes to handlerReadiness

--- chirpy/main.go
+++ chirpy/main.go
@@ -1,29 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
-	mux := http.NewServeMux()
-	server := http.Server{}
 
+	mux := http.NewServeMux()
 	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
 	mux.HandleFunc("/healthz", handlerReadiness)
 
-	server.Addr = ":" + port
-	server.Handler = mux
-
-	err := server.ListenAndServe()
-	if err != nil {
-		fmt.Printf("Server cannot listen : %s", err)
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
 	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
 }
 
-func handlerReadiness(w http.ResponseWriter, request *http.Request) {
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
