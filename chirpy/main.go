809c0f11bc169e4cbf7d4106cf2a77089ca25a49
Just1a2Noob <kamikira.hd@gmail.com>
2025-09-08 11:40:57 +0700

    Changed mux handler to handleFunc

--- chirpy/main.go
+++ chirpy/main.go
@@ -6,16 +6,25 @@ import (
 )
 
 func main() {
+	const filepathRoot = "."
+	const port = "8080"
 	mux := http.NewServeMux()
 	server := http.Server{}
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", handlerReadiness)
 
-	server.Addr = ":8080"
+	server.Addr = ":" + port
 	server.Handler = mux
 
 	err := server.ListenAndServe()
 	if err != nil {
 		fmt.Printf("Server cannot listen : %s", err)
 	}
+}
+
+func handlerReadiness(w http.ResponseWriter, request *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
