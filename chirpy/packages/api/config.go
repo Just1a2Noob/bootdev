174b3ff07bb8d6eae087b7d740ac24df4acf5590
Just1a2Noob <kamikira.hd@gmail.com>
2025-09-16 12:50:10 +0700

    Adjusted function placement for better structure

--- chirpy/packages/api/config.go
+++ chirpy/packages/api/config.go
@@ -1,11 +1,16 @@
 package api
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"sync/atomic"
+	"time"
 
 	"github.com/Just1a2Noob/bootdev/chirpy/internal/database"
+	"github.com/google/uuid"
 )
 
 // Config holds application configuration
@@ -40,4 +45,48 @@ func (cfg *ApiConfig) HandlerResetHits(w http.ResponseWriter, r *http.Request) {
 	cfg.FileserverHits.Store(0)
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Hits reset to 0"))
+}
+
+func SuccessResponse(w http.ResponseWriter, req *chirpRequest, cfg *ApiConfig) {
+	cleaned_text := ProfaneChirp(req.Body)
+	response := chirpRequest{
+		Body: cleaned_text,
+		User: req.User,
+	}
+
+	data, err := json.Marshal(response)
+	if err != nil {
+		ErrorResponse(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// Saves chirpReq in chirps database
+	_, err = cfg.Database.CreateChirps(context.Background(), database.CreateChirpsParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Body:      req.Body,
+		UserID:    uuid.MustParse(req.User),
+	})
+	if err != nil {
+		ErrorResponse(w, "Error inserting chirp to database", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
+}
+
+func ErrorResponse(w http.ResponseWriter, message string, code int) {
+	response := APIResponse{Error: message}
+
+	data, err := json.Marshal(response)
+	if err != nil {
+		log.Printf("Error marshaling error response : %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"error": "internal server error"}`))
+	}
+
+	w.WriteHeader(code)
+	w.Write(data)
 }
