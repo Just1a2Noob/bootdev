295652df0f7afa15e935824158b90214282ea965
Just1a2Noob <kamikira.hd@gmail.com>
2025-10-01 20:22:49 +0700

    Added MakeRefreshToken

---
+++ chirpy/internal/auth/refresh_token.go
@@ -0,0 +1,19 @@
+package auth
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+)
+
+func MakeRefreshTokens() (string, error) {
+	// NOTE: Refresh tokens are used to give permission to refresh the JWT token
+	key := make([]byte, 32)
+	_, err := rand.Read(key)
+	if err != nil {
+		return "", err
+	}
+
+	hex_enc := hex.EncodeToString(key)
+
+	return hex_enc, nil
+}
